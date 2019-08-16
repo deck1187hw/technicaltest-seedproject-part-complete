b24687cdef4c1db5c15a4cf3639f81afedf085c9
Miguel Puig <Miguel.PuigGarcia@ig.com>
2019-08-16 14:37:17 +0100

    find str array refactored

--- src/scripts/FilterDeals.js
+++ src/scripts/FilterDeals.js
@@ -1,10 +1,11 @@
 class FilterDeals {
     constructor(store) {
         this.store = store;
-  
     }
-    findFilter(productTypes = [], element = ''){
-        return true
+    findFilter(productTypes = [], element = ""){
+        return productTypes.find(function(ptype){
+            return ptype.toLowerCase().includes(element.toLowerCase());
+        }) || null
     }
   }
   export default FilterDeals;
--- src/scripts/__tests__/FilterDeals.js
+++ src/scripts/__tests__/FilterDeals.js
@@ -3,12 +3,22 @@ import FilterDeals from "../FilterDeals";
 import mockData from "../../../public/db.json";
 
 describe("filter", () => {
-    it('should find the string on the array of product types', () => {
-
-        const sut = new Store();
-        const filterDeals = new FilterDeals(sut);
-        const result = filterDeals.findFilter(['Broadband', 'Phone'], 'Broadband')
-
-        expect(result).toEqual(true);
-      });
+  it("should find the string on the array of product types", () => {
+    const sut = new Store();
+    const filterDeals = new FilterDeals(sut);
+    const result = filterDeals.findFilter(["Broadband", "Phone"], "Broadband");
+    expect(result).toEqual("Broadband");
+  });
+  it("should find the string on the array of product types even if its lowercase", () => {
+    const sut = new Store();
+    const filterDeals = new FilterDeals(sut);
+    const result = filterDeals.findFilter(["Broadband", "Phone"], "broadband");
+    expect(result).toEqual("Broadband");
+  });
+  it("should return null when no element found", () => {
+    const sut = new Store();
+    const filterDeals = new FilterDeals(sut);
+    const result = filterDeals.findFilter(["Broadband", "Phone"], "NOT EXISTING");
+    expect(result).toEqual(null);
+  });
 });
